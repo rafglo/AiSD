2d9016e6f69507284a40980210c8814048e46ce9
rafglo <276020@student.pwr.edu.pl>
2024-01-03 16:12:28 +0100

    zadanie 5 nareszccie

--- test.py
+++ test.py
@@ -21,13 +21,13 @@ class TreeNode:
         return self.value
 
     def add_left_child(self, value):
-        self.left_child(TreeNode(value))
+        self.left_child = value
 
     def add_right_child(self, value):
-        self.add_right_child(TreeNode(value))
+        self.right_child = value
 
     def get_left_child(self):
-        if self.has_left_child is not None:
+        if self.has_left_child:
             return self.left_child
         else:
             raise ChildError("Left child doesn't exist")
@@ -51,17 +51,32 @@ class TreeNode:
             return False
 
     def is_leaf(self) -> bool:
-        if self.has_left_child or self.has_right_child:
-            return True
-        else:
+        if self.has_left_child() or self.has_right_child():
             return False
-
+        else:
+            return True
+        
     def __str__(self):
-        return str(self.value)
+        funcs = ['sin', 'cos', 'exp', 'log']
+        ops = ['+', '-','*','/','^']
+        if self.left_child is None:
+            a = ""
+        else:
+            a = str(self.left_child)
+        if self.right_child is None:
+            b = ""
+        else:
+            b = str(self.right_child)
+        if self.value in ops:
+            return "("+a+str(self.value)+b+")"
+        elif self.value in funcs:
+            return str(self.value) + "(" + b + ")"
+        else:
+            return a+str(self.value)+b
     
 
 def tokenize(raw):
-    SYMBOLS = set('+-x*/() ') # allow for '*' or 'x' for multiplication
+    SYMBOLS = set('+-*^/() ') # allow for '*' or 'x' for multiplication
     mark = 0
     tokens = []
     n = len(raw)
@@ -76,24 +91,90 @@ def tokenize(raw):
         tokens.append(raw[mark:n]) # complete preceding token
     return tokens
 
+
+print(tokenize("(sin((4*x))/7)"))
+
 def build_expression_tree(tokens):
     S = [] # we use Python list as stack
+    funcs = ["sin", "cos", "exp", "log"]
     for t in tokens:
-        if t in '+-x*/': # t is an operator symbol
+        if t in '+-*/^': # t is an operator symbol
             S.append(t) # push the operator symbol
         elif t not in '()': # consider t to be a literal
-            S.append(TreeNode(t)) # push trivial tree storing value
+            if t in funcs:
+                S.append(t)
+            else:
+                S.append(TreeNode(t)) # push trivial tree storing value
         elif t == ')': # compose a new tree from three constituent parts
             right = S.pop() # right subtree as per LIFO
             op = S.pop() # operator symbol
-            left = S.pop() # left subtree
-            S.append(TreeNode(op, left, right)) # repush tree
+            if op in funcs:
+                S.append(TreeNode(op, None, right))
+            else:
+                left = S.pop() # left subtree
+                S.append(TreeNode(op, left, right)) # repush tree
             # we ignore a left parenthesis
-    return S
+    return S.pop()
 
-def derivative(expression):
+print(build_expression_tree(tokenize("(sin(((x^2)+2))/2)")))
+
+def derivative_on_tree(tree, var):
+    if tree.is_leaf():
+        if tree.value == var:
+            return "1"
+        else:
+            return "0"
+    else:
+        if tree.value == '*':
+            new_tree = TreeNode("+")
+            new_tree.add_left_child(TreeNode('*', derivative_on_tree(tree.get_left_child(), var), tree.get_right_child()))
+            new_tree.add_right_child(TreeNode('*', tree.get_left_child(), derivative_on_tree(tree.get_right_child(), var)))
+            return new_tree
+        elif tree.value == "+":
+            new_tree = TreeNode("+")
+            new_tree.add_left_child(derivative_on_tree(tree.get_left_child(),var))
+            new_tree.add_right_child(derivative_on_tree(tree.get_right_child(),var))
+            return new_tree
+        elif tree.value == "-":
+            new_tree = TreeNode("-")
+            new_tree.add_left_child(derivative_on_tree(tree.get_left_child(),var))
+            new_tree.add_right_child(derivative_on_tree(tree.get_right_child(),var))
+            return new_tree
+        elif tree.value == "/":
+            new_tree = TreeNode("/")
+            new_tree.add_left_child(TreeNode("-", TreeNode("*", derivative_on_tree(tree.get_left_child(), var), tree.get_right_child()), TreeNode("*", tree.get_left_child(), derivative_on_tree(tree.get_right_child(), var))))
+            new_tree.add_right_child(TreeNode("*", tree.get_right_child(), tree.get_right_child()))
+            return new_tree
+        elif tree.value == "^":
+            new_tree = TreeNode("*")
+            new_tree.add_left_child(derivative_on_tree(tree.get_left_child(),var))
+            new_tree.add_right_child(TreeNode("*",tree.get_right_child(),TreeNode("^",tree.get_left_child(),TreeNode("-",tree.get_right_child(),"1"))))
+            return new_tree
+        elif tree.value == "sin":
+            new_tree = TreeNode("*")
+            new_tree.add_left_child(derivative_on_tree(tree.get_right_child(), var))
+            new_tree.add_right_child(TreeNode("cos", None, tree.get_right_child()))
+            return new_tree
+        elif tree.value == "cos":
+            new_tree = TreeNode("*")
+            new_tree.add_left_child(derivative_on_tree(tree.get_right_child(), var))
+            new_tree.add_right_child(TreeNode("*", TreeNode("-", "0", "1"), TreeNode("sin", None, tree.get_right_child())))
+            return new_tree
+        elif tree.value == "exp":
+            new_tree = TreeNode("*")
+            new_tree.add_left_child(derivative_on_tree(tree.get_right_child(),var))
+            new_tree.add_right_child(TreeNode("exp",None,tree.get_right_child()))
+            return new_tree
+        elif tree.value == "log":
+            new_tree = TreeNode("*")
+            new_tree.add_left_child(derivative_on_tree(tree.get_right_child(),var))
+            new_tree.add_right_child(TreeNode("/","1",tree.get_right_child()))
+            return new_tree
+
+    
+def derivative(expression, var):
     tokens = tokenize(expression)
     tree = build_expression_tree(tokens)
-    
+    return derivative_on_tree(tree, var)
 
-derivative("((5+4)*7)/9")
+print(derivative("(x + (sin((x^2)) / x))", "x"))
