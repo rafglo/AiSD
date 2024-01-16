2665f39817fc3f2e940825ca5538e33a72ed7633
rafglo <276020@student.pwr.edu.pl>
2024-01-16 23:04:12 +0100

    zad 3,4

--- test.py
+++ test.py
@@ -1,15 +1,51 @@
-import graphviz
-dot = graphviz.Digraph() 
-dot.node('A', 'King Arthur')  # doctest: +NO_EXE
-dot.node('B', 'Sir Bedevere the Wise')
-dot.node('L', 'Sir Lancelot the Brave')
+from collections import defaultdict
+class Graph:
+    def __init__(self, directed=False):
+        self.graph = defaultdict(list)
+        self.directed = directed
 
-dot.edges(['AB', 'AL'])
-dot.edge('B', 'L', constraint='false')
+    def Edge(self, frm, to):
+        self.graph[frm].append(to)
 
-print(dot.source)
+        if self.directed is False:
+            self.graph[to].append(frm)
+        else:
+            self.graph[to] = self.graph[to]
 
-doctest_mark_exe()
+    def visit(self, s, visited, sortlist):
+        visited[s] = True
 
-dot.render('doctest-output/round-table.gv').replace('\\', '/')
-'doctest-output/round-table.gv.pdf'
+        for i in self.graph[s]:
+            if not visited[i]:
+                self.visit(i, visited, sortlist)
+
+        sortlist.insert(0, s)
+
+    def topological_Sort(self):
+        visited = {i: False for i in self.graph}
+
+        sortlist = []
+       
+        for v in self.graph:
+            if not visited[v]:
+                self.visit(v, visited, sortlist)
+
+        print(sortlist)
+
+#driver code
+if __name__ == '__main__':
+ 
+    g = Graph(directed=True)
+
+    g.Edge(0, 1)
+    g.Edge(0, 2)
+    g.Edge(1, 2)
+    g.Edge(2, 0)
+    g.Edge(2, 3)
+    g.Edge(3, 4)
+    g.Edge(4, 7)
+    g.Edge(7, 8)
+    g.Edge(2,12)
+    
+    print("The Result after Topological Sort:")
+    g.topological_Sort()
--- test2.py
+++ test2.py
@@ -1,3 +1,4 @@
+import graphviz, sys
 class Vertex():
     def __init__(self, key):
         self.id = key
@@ -18,6 +19,70 @@ class Vertex():
     def get_weight(self, neighbor):
         return self.neighbors[neighbor]
 
+
+class Empty(Exception):
+    pass
+
+class Queue:
+    DEFAULT_CAPACITY = 10
+    K = 0.2
+
+    def __init__(self):
+        self._data = [None]*Queue.DEFAULT_CAPACITY
+        self._size = 0
+        self._front = 0
+
+    def __len__(self):
+        return self._size
+
+    def is_empty(self):
+        return self._size == 0
+
+    def first(self):
+        if self.is_empty():
+            raise Empty('Queue is empty')
+        return self._data[self._front]
+
+    def dequeue(self):
+        if self.is_empty():
+            raise Empty('Queue is empty')
+        value = self._data[self._front]
+        self._data[self._front] = None
+        self._front = (self._front+1)%len(self._data)
+        self._size -= 1
+        if self._size <= Queue.K * len(self._data):
+            self.resize(len(self._data) // 2)
+        return value
+
+    def enqueue(self,e):
+        if self._size == len(self._data):
+            self.resize(2*len(self._data))
+        avail = (self._front + self._size)%len(self._data)
+        self._data[avail] = e
+        self._size += 1
+
+    def resize(self,cap):
+        old = self._data
+        self._data = [None]*cap
+        walk = self._front
+        for k in range(self._size): #only existing elements
+            self._data[k] = old[walk]
+            walk = (1 + walk)%len(old)
+        self._front = 0 
+
+    def __str__(self):
+        """elements = [(self._data[(self._front + i) % len(self._data)]) for i in range(self._size)]
+        return f"Queue: {elements}"
+        """
+        result = []
+        for i in range(len(self._data)):
+            if self._data[(self._front + i)%len(self._data)] is None:
+                result.append(" ")
+            else:
+                result.append(self._data[(self._front + i)%len(self._data)])
+        return str(result)
+
+from collections import deque
 
 class Graph(Vertex):
     def __init__(self):
@@ -56,15 +121,75 @@ class Graph(Vertex):
         self.vertices[from_key].add_neighbor(self.vertices[to_key], weight)
     
     def get_edges(self):
-        edges = {}
+        edges = []
         for vert in self.vertices:
             neighbors = self.vertices[vert].get_neighbors()
             if not neighbors is None:
                 for n in neighbors:
-                    edges[vert] = n
+                    edges.append(str(vert) + str(n))
         return edges
+    
+    def visualize(self):
+        dot = graphviz.Digraph()
+        for vert in self.vertices:
+            dot.node(str(vert), str(vert))
+        for edge in self.get_edges():
+            dot.edge(str(edge[0]), str(edge[1]))
+        return dot
+        
+    def bfs(self, start):
+        visited = [start]
+        q = []
+        q.append(start)
+        while len(q):
+            vert = q.pop()
+            neighbors = self.vertices[vert].get_neighbors()
+            for neighbor in neighbors:
+                if not neighbor in visited:
+                    q.append(neighbor)
+                    visited.append(neighbor)
+        return visited
+
+    def dfs(self, start, visited = []):
+        if not start in visited:
+            visited.append(start)
+        neighbors = self.vertices[start].get_neighbors()
+        for neighbor in neighbors:
+            if not neighbor in visited:
+                self.dfs(neighbor, visited)
+        return visited
+
+    def dfs_topological_sort(self, start, stack, visited = []):
+        if not start in visited:
+            visited.append(start)
+        neighbors = self.vertices[start].get_neighbors()
+        for neighbor in neighbors:
+            if not neighbor in visited:
+                self.dfs_topological_sort(neighbor, stack, visited)
+        stack.append(start)
+
+    def topological_sort(self):
+        visited = []
+        stack = []
+
+        for vert in self.get_vertices():
+            if not vert in visited:
+                self.dfs_topological_sort(vert, stack, visited)
+        
+        return stack[::-1]
 
 g = Graph()
-g.add_vertex("a")
-g.add_edge("a", "b")
-print(g.get_edges())
+g.add_edge(0, 1)
+g.add_edge(0, 2)
+g.add_edge(1, 2)
+g.add_edge(2, 0)
+g.add_edge(2, 3)
+g.add_edge(3, 4)
+g.add_edge(4,6)
+g.add_edge(6,9)
+g.add_edge(6,10)
+g.add_edge(2, 5)
+print(g.bfs(2))
+print(g.dfs(2))
+g.visualize()
+print(g.topological_sort())
